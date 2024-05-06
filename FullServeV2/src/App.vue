34272079c119d287a19453e3ec696c692409e3a4
Jordan Haddadi <jordanhaddadi@gmail.com>
2024-05-06 21:19:28 +0000

    Misc

--- FullServeV2/src/App.vue
+++ FullServeV2/src/App.vue
@@ -1,17 +1,8 @@
-<script setup>
-import DestinationCard from './components/DestinationCard.vue'
-import Navbar from './components/Navbar.vue'
-import PropertyCard from './components/PropertyCard.vue'
-</script>
-
 <template>
   <div id="app" class="antialiased">
     HELLLLOOO
     <component :is="this.$route.meta.layout">
       <router-view />
-      <DestinationCard />
-      <Navbar />
-      <PropertyCard />
     </component>
   </div>
 </template>
